15124d0c007c8f1d085a1f58713ba63cfd023ab9
carlinmkk <carluseduardobarreto@gmail.com>
2026-05-04 13:00:36 -0300

    up

--- CienciaDaComputacao/Semestre1/IntroducaoProgramacao/list7/Uni5Exe02.java
+++ CienciaDaComputacao/Semestre1/IntroducaoProgramacao/list7/Uni5Exe02.java
@@ -6,19 +6,17 @@ public class Uni5Exe02 {
 
     public static void main(String[] args) {
 
-        int vetNumeros[] = new int[100];
         int somaPares = 0;
         int somaImpares = 0;
 
         try (Scanner sc = new Scanner(System.in)) {
-            for (int i = 0; i < vetNumeros.length; i++) {
+            for (int i = 0; i <= 100; i++) {
 
                 if (i % 2 == 0) {
-                     vetNumeros[i] += vetNumeros[i];
-                     somaPares = vetNumeros[i];
+                    somaPares += i;
                 } else {
-                    vetNumeros[i] += vetNumeros[i];
-                     somaImpares = vetNumeros[i];
+
+                    somaImpares += i;
                 }
             }
             System.out.println("Soma dos numeros pares: " + somaPares);
