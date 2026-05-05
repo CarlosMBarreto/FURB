3919d5f35c8dba09a8084bfe6ceb3135cfda068d
carlinmkk <carluseduardobarreto@gmail.com>
2026-05-05 00:17:44 -0300

    up

---
+++ CienciaDaComputacao/Semestre1/IntroducaoProgramacao/list7/Uni5Exe07.java
@@ -0,0 +1,20 @@
+package list7;
+
+import java.util.Scanner;
+
+public class Uni5Exe07 {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+
+            int n = sc.nextInt();
+            double maior = 0;
+            double medio = 0;
+            double menor = 0;
+
+            for(int i = 0;i < n; i++){
+                System.out.println("Informe o " + i + "º número: ");
+            }
+        }
+    }
+}
