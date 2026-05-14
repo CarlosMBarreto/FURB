3ba524f5cca0ebfb3902b4a3a71ff2ad92d4aa64
carlinmkk <carluseduardobarreto@gmail.com>
2026-05-14 16:05:24 -0300

    list8

---
+++ CienciaDaComputacao/Semestre1/IntroducaoProgramacao/list8/Uni6Exe04.java
@@ -0,0 +1,49 @@
+package list8;
+
+import java.util.Scanner;
+
+public class Uni6Exe04 {
+
+    public static void main(String[] args) {
+        int[] v1 = new int[3];
+        int[] v2 = new int[3];
+        int[] v3 = new int[3];
+
+        lerValores(v1, v2);
+        somaVetores(v1, v2, v3);
+        escreverValores(v1, v2, v3);
+    }
+
+    public static void lerValores(int[] v1, int[] v2) {
+        try (Scanner sc = new Scanner(System.in)) {
+            for (int i = 0; i < v1.length; i++) {
+                v1[i] = sc.nextInt();
+            }
+            for (int i = 0; i < v2.length; i++) {
+                v2[i] = sc.nextInt();
+            }
+
+        }
+    }
+
+    public static void somaVetores(int[] v1, int[] v2, int[] v3) {
+        for (int i = 0; i < v3.length; i++) {
+            v3[i] = v1[i] + v2[i];
+        }
+    }
+
+    public static void escreverValores(int[] v1, int[] v2, int[] v3) {
+        System.out.print("Vetor1 = ");
+        for (int i = 0; i < v1.length; i++) {
+            System.out.print("[" + v1[i] + "]");
+        }
+        System.out.print("\nVetor2 = ");
+        for (int i = 0; i < v2.length; i++) {
+            System.out.print("[" + v2[i] + "]");
+        }
+        System.out.print("\nVetor3 = ");
+        for (int i = 0; i < v3.length; i++) {
+            System.out.print("[" + v3[i] + "]");
+        }
+    }
+}
---
+++ CienciaDaComputacao/Semestre1/IntroducaoProgramacao/list8/Uni6Exe05.java
@@ -0,0 +1,54 @@
+package list8;
+
+import java.util.Scanner;
+
+public class Uni6Exe05 {
+
+    public static void main(String[] args) {
+        String[] respostaRapaz = new String[5];
+        String[] respostaMoca = new String[5];
+
+        lerRespostas(respostaRapaz, respostaMoca);
+        afinidade(respostaRapaz, respostaMoca);
+    }
+
+    public static void lerRespostas(String[] respostaRapaz, String[] respostaMoca) {
+        try (Scanner sc = new Scanner(System.in)) {
+            for (int i = 0; i < respostaRapaz.length; i++) {
+                respostaRapaz[i] = sc.nextLine();
+            }
+            for (int i = 0; i < respostaMoca.length; i++) {
+                respostaMoca[i] = sc.nextLine();
+            }
+        }
+    }
+
+    public static void afinidade(String[] respostaRapaz, String[] respostaMoca) {
+
+        int afinidade = 0;
+
+        for (int i = 0; i < respostaRapaz.length; i++) {
+            if ((respostaRapaz[i].equals("SIM") && respostaMoca[i].equals("SIM")) || (respostaRapaz[i].equals("NÃO") && respostaMoca[i].equals("NÃO")) || (respostaRapaz[i].equals("IND") && respostaMoca[i].equals("IND"))) {
+                afinidade += 3;
+            } else if (respostaRapaz[i].equals("IND") || respostaMoca[i].equals("IND")){
+                afinidade += 1;
+            } else if ((respostaRapaz[i].equals("SIM") && respostaMoca[i].equals("NÃO")) || (respostaRapaz[i].equals("NÃO") && respostaMoca[i].equals("SIM"))){
+                afinidade -= 2;
+            }
+        }
+        
+        if(afinidade == 15){
+            System.out.println("Casem!");
+        } else if (afinidade > 9 && afinidade <= 14){
+            System.out.println("Vocês têm muita coisa em comum!");
+        } else if (afinidade > 4 && afinidade <= 9){
+            System.out.println("Talvez não dê certo :(");
+        } else if (afinidade > -1 && afinidade <= 4){
+            System.out.println("Vale um encontro.");
+        } else if (afinidade >= -9 && afinidade < -1){
+            System.out.println("Melhor não perder tempo");
+        } else if (afinidade == -10){
+            System.out.println("Vocês se odeiam!");
+        }
+    }
+}
