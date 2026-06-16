d4522c3fa77e8e48e5e783f5a18ac5dcb78abd8b
CarlosMBarreto <carluseduardobarreto@gmail.com>
2026-06-16 17:04:56 -0300

    TrabalhoFinal

--- BCC/Semestre1/IntroducaoProgramacao/ProjetoFinal/Main.java
+++ BCC/Semestre1/IntroducaoProgramacao/ProjetoFinal/Main.java
@@ -1,6 +1,5 @@
 package ProjetoFinal;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -9,7 +8,9 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        String sala[][] = new String[10][10];
+        Sala sala = new Sala();
+
+        sala.randomizarAssentos();
 
         int opcao = 0;
 
@@ -24,23 +25,33 @@ public class Main {
 
             switch (opcao) {
                 case 1:
-                    mostrarSala(sala);
+                    sala.exibirSala();
                     break;
 
                 case 2:
+                    System.out.println("===== Cine Campus - Totem de autoatendimento =====");
+                    sala.exibirSala();
+
                     System.out.print("Fila: ");
                     int fila = sc.nextInt();
 
                     System.out.print("Poltrona: ");
                     int poltrona = sc.nextInt();
 
-                    System.out.print("Tipo: [1]Inteira - [2]Meia: ");
-                    int tipo = sc.nextInt();
+                    if (sala.posicaoValida(fila, poltrona)) {
+                        System.out.print("Tipo - Inteira[1] / Meia[2]: ");
+                        int tipo = sc.nextInt();
 
-                    
+                        if (tipo != 1 && tipo != 2) {
+                            System.out.println("Entrada Incorreta.");
+                        } else {
+                            sala.reservar(fila, poltrona, tipo, sc);
+                        }
+                    }
                     break;
 
                 case 3:
+                    sala.resumo();
                     break;
 
                 case 0:
@@ -51,53 +62,9 @@ public class Main {
                     System.out.println("Opção Incorreta.");
                     break;
             }
-        } while (opcao != 0);
-
-    }
-
-    public static void mostrarSala(String sala[][]) {
-
-        Random rand = new Random();
-
-        int preenchidos = 0;
-        int ingressosVendidos = 0;
-        double bilheteria = 150.6789;
-        int ocupacao = 0;
-
-        System.out.println("===== Cine Campus - Totem de autoatendimento =====");
-        System.out.print("Ingressos vendidos: " + ingressosVendidos + "\t");
-        System.out.printf("Bilheteria: %.2f", bilheteria);
-        System.out.println("\tOcupação: " + ocupacao + "%");
-
-        System.out.println("  0  1  2  3  4  5  6  7  8  9");
-
-        for (int i = 0; i < sala.length; i++) {
-            for (int j = 0; j < sala[i].length; j++) {
-                sala[i][j] = " ";
-            }
+        } while (opcao != 0 && sala.ingressosVendidos() < 85);
+        if (sala.ingressosVendidos() == 85) {
+            System.out.println("Sessão lotada!");
         }
-
-        while (preenchidos < 15) {
-            int lin = rand.nextInt(sala.length);
-            int col = rand.nextInt(sala[0].length);
-
-            if (sala[lin][col] == " ") {
-                sala[lin][col] = "#";
-            }
-            preenchidos++;
-        }
-
-        for (int i = 0; i < sala.length; i++) {
-            System.out.print(i);
-            for (int j = 0; j < sala[i].length; j++) {
-                if (!sala[i][j].equals("#")) {
-                    System.out.print("[" + "-" + "]");
-                } else {
-                    System.out.print("[" + sala[i][j] + "]");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println(" ------------ TELA ------------  \n\n");
     }
 }
---
+++ BCC/Semestre1/IntroducaoProgramacao/ProjetoFinal/Sala.java
@@ -0,0 +1,174 @@
+package ProjetoFinal;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Sala {
+    private boolean assentos[][] = new boolean[10][10];
+    private double bilheteria = 0;
+    private final int assentosOcupados = 15;
+
+    public Sala() {
+    }
+
+    public void randomizarAssentos() {
+        Random rand = new Random();
+
+        int preenchidos = 0;
+
+        while (preenchidos < assentosOcupados) {
+            int i = rand.nextInt(assentos.length);
+            int j = rand.nextInt(assentos[0].length);
+
+            if (!assentos[i][j]) {
+                assentos[i][j] = true;
+                preenchidos++;
+            }
+        }
+    }
+
+    public void exibirSala() {
+
+        System.out.println("\t ------------ TELA ------------  ");
+        System.out.print("\t");
+        for (int i = 0; i < assentos.length; i++) {
+            System.out.print("  " + i);
+        }
+        System.out.println();
+
+        for (int i = 0; i < assentos.length; i++) {
+            System.out.print("\t" + i);
+            for (int j = 0; j < assentos[i].length; j++) {
+                if (assentos[i][j]) {
+                    System.out.print("[#]");
+                } else {
+                    System.out.print("[-]");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean posicaoValida(int fileira, int coluna) {
+        if (fileira < 0 || fileira >= assentos.length) {
+            System.out.println("Fora do limite.");
+            return false;
+        } else if (coluna < 0 || coluna >= assentos[0].length) {
+            System.out.println("Fora do limite.");
+            return false;
+        }
+        return true;
+    }
+
+    public void reservar(int fileira, int coluna, int tipo, Scanner sc) {
+        if (posicaoValida(fileira, coluna)) {
+            if (assentos[fileira][coluna]) {
+                System.out.println("Assento já ocupado!");
+            } else {
+                double preco = calcularPreco(fileira, tipo);
+
+                String setor = "";
+
+                char r;
+
+                if (fileira >= 0 && fileira < 2) {
+                    setor = "Poltrona(frente)";
+                } else if (fileira >= 2 && fileira < 8) {
+                    setor = "Poltrona(meio)";
+                } else if (fileira >= 8 && fileira <= 9) {
+                    setor = "Poltrona(VIP)";
+                }
+
+
+                System.out.println(setor);
+                System.out.printf("Valor: R$ %.2f\n", preco);
+                System.out.println("Confirmar? (S/N): ");
+                r = sc.next().toUpperCase().charAt(0);
+
+                if (r == 'S') {
+                    assentos[fileira][coluna] = true;
+                    System.out.println("Compra realizada!");
+                    bilheteria += preco;
+                } else {
+                    System.out.println("Reserva cancelada.");
+                }
+            }
+        }
+
+    }
+
+    public double percentualOcupacao() {
+        int ocupados = 0;
+
+        for (int i = 0; i < assentos.length; i++) {
+            for (int j = 0; j < assentos[i].length; j++) {
+                if (assentos[i][j]) {
+                    ocupados++;
+                }
+            }
+        }
+
+        double porcentagem = (ocupados / (double) (assentos.length * assentos[0].length)) * 100;
+
+        return porcentagem;
+    }
+
+    public int ingressosVendidos() {
+
+        int ingressos = 0;
+
+        for (int i = 0; i < assentos.length; i++) {
+            for (int j = 0; j < assentos[i].length; j++) {
+                if (assentos[i][j]) {
+                    ingressos++;
+                }
+            }
+        }
+
+        return ingressos - assentosOcupados;
+    }
+
+    public double getBilheteria() {
+        return bilheteria;
+    }
+
+    public String classificacao(double porcentagem) {
+
+        if (porcentagem >= 0 && porcentagem < 40) {
+            return "Sala Vazia - precisa divulgar mais";
+        } else if (porcentagem >= 40 && porcentagem < 70) {
+            return "Sessão Mediana";
+        } else if (porcentagem >= 70 && porcentagem < 90) {
+            return "Casa Cheia";
+        }
+        return "Sessão Esgotada - Sucesso de bilheteria!";
+    }
+
+    public double calcularPreco(int fileira, int tipo) {
+        double preco = 0;
+
+        if (fileira >= 0 && fileira < 2) {
+            preco = 15;
+        } else if (fileira >= 2 && fileira < 8) {
+            preco = 25;
+        } else if (fileira >= 8 && fileira <= 9) {
+            preco = 35;
+        }
+
+        if (tipo == 2) {
+            preco /= 2;
+        }
+
+        return preco;
+    }
+
+    public void resumo() {
+        double ocupacao = percentualOcupacao();
+
+        System.out.println("=== RESUMO ===");
+        System.out.println("Ingressos vendidos: " + ingressosVendidos());
+        System.out.printf("Bilheteria: R$ %.2f\n", getBilheteria());
+        System.out.printf("Ocupação: %.2f%%\n", ocupacao);
+        System.out.println(classificacao(ocupacao));
+    }
+}
